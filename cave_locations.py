91990e180c1d075827797a55bd660e644bb49d05
mikr ess <mikress@mikrs-Mac-Studio.local>
2024-10-17 12:54:28 -0500

    this commit updats the repo

---
+++ cave_locations.py
@@ -0,0 +1,26 @@
+# 18 5 15 (153, 182)
+# the local poisiton is 18 6 15 (154, 182)
+# the local poisiton is 1
+global_entrance = (147,181)
+
+first_Right = (134,181)
+first_left = (134,188)
+
+second_Right = (122,188)
+second_left = (122,204)
+
+third_left =  (114,204)
+
+fourth_left = (114,182)
+
+fifth_left = (127,182)
+
+third_Right = (127,184)
+
+fourth_Right = (131,184)
+
+fifth_right = (131,177)
+
+sixth_left= (117,177)
+
+enter = (117,172)
