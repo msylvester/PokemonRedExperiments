3d158eea06750b506976c2f4652c505873a84414
mikr ess <mikress@mikrs-Mac-Studio.local>
2024-10-22 10:10:33 -0500

    working  versions of the subclass

---
+++ baselines/parentV2imp.py
@@ -0,0 +1,132 @@
+from parentV2 import RedGymEnv
+import numpy as np
+
+event_flags_start = 0xD747
+event_flags_end = 0xD7F6 # 0xD761 # 0xD886 temporarily lower event flag range for obs input
+museum_ticket = (0xD754, 0)
+
+class CustomRedGymEnv(RedGymEnv):
+    def __init__(self, env_conf):
+        super().__init__(env_conf)
+        # Add any custom initialization code here if needed
+    
+    def _get_obs(self):
+        
+        screen = self.render()
+
+        self.update_recent_screens(screen)
+        
+        # normalize to approx 0-1
+        level_sum = 0.02 * sum([
+            self.read_m(a) for a in [0xD18C, 0xD1B8, 0xD1E4, 0xD210, 0xD23C, 0xD268]
+        ])
+
+        observation = {
+            "screens": self.recent_screens,
+            "health": np.array([self.read_hp_fraction()]),
+            "level": self.fourier_encode(level_sum),
+            "badges": np.array([int(bit) for bit in f"{self.read_m(0xD356):08b}"], dtype=np.int8),
+            "events": np.array(self.read_event_bits(), dtype=np.int8),
+            "map": self.get_explore_map()[:, :, None],
+            "recent_actions": self.recent_actions
+        }
+
+        return observation
+
+    def reset(self, seed=None, options={}):
+        self.seed = seed
+        # restart game, skipping credits
+        with open(self.init_state, "rb") as f:
+            self.pyboy.load_state(f)
+
+        self.init_map_mem()
+
+        self.agent_stats = []
+
+        self.explore_map_dim = 384
+        self.explore_map = np.zeros((self.explore_map_dim,self.explore_map_dim), dtype=np.uint8)
+
+        self.recent_screens = np.zeros( self.output_shape, dtype=np.uint8)
+        
+        self.recent_actions = np.zeros((self.frame_stacks,), dtype=np.uint8)
+
+        self.levels_satisfied = False
+        self.base_explore = 0
+        self.max_opponent_level = 0
+        self.max_event_rew = 0
+        self.max_level_rew = 0
+        self.last_health = 1
+        self.total_healing_rew = 0
+        self.died_count = 0
+        self.party_size = 0
+        self.step_count = 0
+
+        self.base_event_flags = sum([
+                self.bit_count(self.read_m(i))
+                for i in range(event_flags_start, event_flags_end)
+        ])
+
+        self.current_event_flags_set = {}
+
+        # experiment! 
+        # self.max_steps += 128
+
+        self.max_map_progress = 0
+        self.progress_reward = self.get_game_state_reward()
+        self.total_reward = sum([val for _, val in self.progress_reward.items()])
+        self.reset_count += 1
+        return self._get_obs(), {}
+  
+    def step(self, action):
+        if action == 7:
+            print(f'action {action}')
+        if self.save_video and self.step_count == 0:
+            self.start_video()
+
+        self.run_action_on_emulator(action)
+        self.append_agent_stats(action)
+
+        self.update_recent_actions(action)
+
+        self.update_seen_coords()
+
+        self.update_explore_map()
+
+        self.update_heal_reward()
+
+        self.party_size = self.read_m(0xD163)
+
+        new_reward = self.update_reward()
+
+        self.last_health = self.read_hp_fraction()
+
+        self.update_map_progress()
+
+        step_limit_reached = self.check_if_done()
+
+        obs = self._get_obs()
+
+        # self.save_and_print_info(step_limit_reached, obs)
+
+        # create a map of all event flags set, with names where possible
+        #if step_limit_reached:
+        if self.step_count % 100 == 0:
+            for address in range(event_flags_start, event_flags_end):
+                val = self.read_m(address)
+                for idx, bit in enumerate(f"{val:08b}"):
+                    if bit == "1":
+                        # TODO this currently seems to be broken!
+                        key = f"0x{address:X}-{idx}"
+                        if key in self.event_names.keys():
+                            self.current_event_flags_set[key] = self.event_names[key]
+                        else:
+                            print(f"could not find key: {key}")
+
+        self.step_count += 1
+
+        return obs, new_reward, False, step_limit_reached, {}
+    
+    # def _take_action(self, action):
+    #     # Custom action handling logic
+    #     # Apply the action and return reward, done, and any additional info
+    #     return reward, done, info
